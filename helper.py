187ad1725511e9a2086378657f488b32ed04ed3d
NikhilNataraj <nikhil.nataraj@gmail.com>
2025-10-02 08:15:04 +0530

    Initialise and use database

--- helper.py
+++ helper.py
@@ -1,37 +1,22 @@
-def get_data(item_id, data):
-    item_id = int(item_id)
-    item = None
-    for i in data:
-        if i['ID'] == item_id:
-            item = {
-                'id': item_id,
-                'description': i['Description'],
-                'amount': i['Amount'],
-                'method': i['Method'],
-                'date': i['Date'],
-            }
-    return item
-
-
 def get_total(arr):
+    """Calculates the sum of amounts from a list of transactions."""
     total = 0
     for t in arr:
-        total += t['Amount']
-
+        # SQLite returns data with lowercase keys
+        total += t['amount']
     return total
 
 
-def pack_data(form_data, item_id):
+def unpack_data(form_data):
+    """Extracts and formats data from a submitted form."""
     description = form_data.get('description')
     amount = float(form_data.get('amount'))
     method = form_data.get('method')
     tran_date = form_data.get('date')
     data = {
-        "ID": item_id,
-        "Date": tran_date,
-        "Description": description,
-        "Amount": amount,
-        "Method": method
+        "date": tran_date,
+        "description": description,
+        "amount": amount,
+        "method": method
     }
-
     return data
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
 
-from flask import Flask, render_template, request, url_for, redirect, session, flash
+from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_bootstrap import Bootstrap
 from flask_login import login_user, LoginManager, login_required, logout_user, UserMixin
 from werkzeug.security import check_password_hash
 from dotenv import load_dotenv
 from datetime import datetime
 
-from helper import get_data, get_total, pack_data
-from API import read_data, update_row, create_row, create_sheet, delete_sheet, delete_row, convert_to_dict
+# UPDATED IMPORTS to use local database and helper functions
+from helper import get_total, unpack_data
+import db as database
 
 load_dotenv()
 
@@ -23,8 +24,8 @@ PASSWORD = os.getenv("PASSWORD")
 
 
 class User(UserMixin):
-    def __init__(self, user_id, email, password):
-        self.id = user_id
+    def __init__(self, id, email, password):
+        self.id = id
         self.email = email
         self.password = password
 
@@ -32,7 +33,7 @@ class User(UserMixin):
 @login_manager.user_loader
 def load_user(user_id):
     if user_id == "1":
-        return User(user_id="1", email=USER, password=PASSWORD)
+        return User(id="1", email=USER, password=PASSWORD)
     return None
 
 
@@ -48,7 +49,7 @@ def login():
         password = request.form.get("password")
 
         if email == USER and check_password_hash(PASSWORD, password):
-            user = User(user_id="1", email=email, password=PASSWORD)
+            user = User(id="1", email=email, password=PASSWORD)
             login_user(user)
             return redirect(url_for("tracker"))
         else:
@@ -61,41 +62,50 @@ def login():
 @app.route("/tracker", methods=["GET", "POST"])
 @login_required
 def tracker():
-    income_data = convert_to_dict(read_data("Income"))
-    expense_data = convert_to_dict(read_data("Expenses"))
+    # REPLACED API calls with much faster local DB calls
+    income_data = database.get_transactions("income")
+    expense_data = database.get_transactions("expense")
 
     total_income = get_total(income_data)
     total_expenses = get_total(expense_data)
-    total_exp_percent = round(total_expenses / total_income * 100)
 
-    month_name = datetime.strptime(income_data[0]["Date"], "%Y-%m-%d").strftime("%B %Y")
+    # Prevent division by zero error
+    total_exp_percent = 0
+    if total_income > 0:
+        total_exp_percent = round(total_expenses / total_income * 100)
+
+    # Display the current month by default
+    month_name = datetime.now().strftime("%B %Y")
+    # If there is income data, use its date to determine the month
+    if income_data:
+        try:
+            month_name = datetime.strptime(income_data[0]["date"], "%Y-%m-%d").strftime("%B %Y")
+        except (ValueError, IndexError):
+            # Handle cases with bad date format or no data
+            pass
+
 
     if request.method == 'POST':
-        action = request.form.get('action')  # Which Button was clicked
+        action = request.form.get('action')
 
         if action == 'income' or action == 'expense':
-            data = pack_data(request.form, len(expense_data))
-            create_row(sheet="Income", info=data) if action == 'income' else create_row(sheet="Expenses", info=data)
-
+            data = unpack_data(request.form)
+            database.add_transaction(action, data)
 
         elif action == 'edit_income':
             item_id = request.form.get('item_id')
-            print(item_id)
-            session['item_data'] = get_data(item_id, income_data)
-            return redirect(url_for('edit', item_id=item_id, tran="Income"))
+            return redirect(url_for('edit', tran_type="income", item_id=item_id))
 
         elif action == 'delete_income':
-            delete_row("Income", request.form.get('item_id'))
+            database.delete_transaction("income", request.form.get('item_id'))
 
         elif action == 'edit_expense':
             item_id = request.form.get('item_id')
-            session['item_data'] = get_data(item_id, expense_data)
-            return redirect(url_for('edit', item_id=item_id, tran="Expenses"))
+            return redirect(url_for('edit', tran_type="expense", item_id=item_id))
 
         elif action == 'delete_expense':
-            delete_row("Income", request.form.get('item_id'))
+            database.delete_transaction("expense", request.form.get('item_id'))
 
-            # Return response or redirect
         return redirect(url_for('tracker'))
 
     return render_template("index.html", income=income_data, expense=expense_data,
@@ -104,20 +114,26 @@ def tracker():
                            month=month_name, logged_in=True)
 
 
-@app.route("/edit/<tran>/<item_id>", methods=["GET", "POST"])
-def edit(tran, item_id):
+@app.route("/edit/<tran_type>/<item_id>", methods=["GET", "POST"])
+@login_required
+def edit(tran_type, item_id):
+    # Fetch the specific item directly from the database
+    item = database.get_single_transaction(tran_type, item_id)
+    if not item:
+        flash("Record not found!")
+        return redirect(url_for('tracker'))
+
     if request.method == "POST":
-        action = request.form.get('action')  # Which Button was clicked
+        action = request.form.get('action')
 
         if action == "save":
-            data = pack_data(request.form, item_id)
-            update_row(sheet=tran, row=f"{int(item_id)+1}", info=data)
+            data = unpack_data(request.form)
+            database.update_transaction(tran_type, item_id, data)
 
         return redirect(url_for("tracker"))
 
-    item = session.pop('item_data', {})
-    item['tran'] = tran
-    return render_template("edit.html", **item)
+    # Pass the database row object and transaction type to the template
+    return render_template("edit.html", item=item, tran_type=tran_type)
 
 
 @app.route("/logout")
